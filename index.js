bf131d2ad7e3ca84d0fa2487d7e629c25c51e5d8
bur1to <daniil.dedenok@gmail.com>
2023-02-06 17:17:35 +0200

    add cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const { userRoute, eventRoute } = require('./routes/index');
 
 mongoose.set('strictQuery', true);
@@ -7,6 +8,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded());
+app.use(cors());
 
 mongoose.connect('mongodb://localhost:27017/userEvents', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Database successfully connected'))
